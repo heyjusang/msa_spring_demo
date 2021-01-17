9d1fe9fe892a792b7ddd5715e5e6138e993b04f9
heyjusang <co9901@gmail.com>
2021-01-17 20:36:04 +0900

    init project

---
+++ src/main/kotlin/hey/jusang/msatest/MsatestApplication.kt
@@ -0,0 +1,11 @@
+package hey.jusang.msatest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MsatestApplication
+
+fun main(args: Array<String>) {
+	runApplication<MsatestApplication>(*args)
+}
---
+++ src/test/kotlin/hey/jusang/msatest/MsatestApplicationTests.kt
@@ -0,0 +1,13 @@
+package hey.jusang.msatest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MsatestApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
